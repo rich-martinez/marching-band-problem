d454a98cab91f8b68b891b328a67b9e9857d97c1
Rich Martinez <richardmartinez@users.noreply.github.com>
2020-08-06 22:55:24 -0400

    Fix float range issue in loop

--- calculate-factors.py
+++ calculate-factors.py
@@ -1,20 +1,18 @@
 import os
 
 minimumNumber = 64
-arbitraryMaximumNumber = 10000
-currentNumber = null
+arbitraryMaximumNumber = 100000
 
 for baseNumber in range(minimumNumber, arbitraryMaximumNumber):
   factors = []
   minimumFactor = 2
-  maximumFactor = (baseNumber / 2)
+  maximumFactor = int(baseNumber / 2)
 
   for factorOfBaseNumber in range(minimumFactor, maximumFactor):
     if baseNumber % factorOfBaseNumber == 0:
       factors.append(factorOfBaseNumber)
 
-  if factors.length == 64
-    currentNumber = baseNumber
+  if len(factors) == 64:
+    print(baseNumber)
     break
-
-os.system(currentNumber)
+  
